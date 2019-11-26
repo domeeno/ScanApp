d303e70106f194d548e7f352b6245ebc8c11dd3c
dominatorfaf <45204549+dominatorfaf@users.noreply.github.com>
2019-11-26 12:47:08 +0200

    Front-end v2

--- app/src/main/java/com/example/scanapp/Main2Activity.java
+++ app/src/main/java/com/example/scanapp/Main2Activity.java
@@ -2,7 +2,11 @@ package com.example.scanapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button scanButton = findViewById(R.id.see_order_btn);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenu();
+                Log.i("Visual", "The switch was successful");
+            }
+        });
+    }
+    public void openMenu() {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
