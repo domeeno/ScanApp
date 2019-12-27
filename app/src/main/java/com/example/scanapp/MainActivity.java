8a4af753697be7607556b0d3e43e840174662015
dominatorfaf <45204549+dominatorfaf@users.noreply.github.com>
2019-12-27 17:16:07 +0200

    Database Connection added.

--- app/src/main/java/com/example/scanapp/MainActivity.java
+++ app/src/main/java/com/example/scanapp/MainActivity.java
@@ -9,29 +9,17 @@ import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.SimpleAdapter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    GridView gridview;
-    ArrayList<String> arrayList;
-
     //for DB
     String ip, db, un, passwords;
     Connection connect;
-    PreparedStatement stmt;
-    ResultSet rs;
+//    PreparedStatement stmt;
+//    ResultSet rs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Button to the next page
         Button scanButton = findViewById(R.id.scan_btn);
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,55 +37,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //db connection
         ip = "localhost";
-        un = "appUser";
-        passwords = "1234";
-        db = "Restaurant";
-        gridview = (GridView) findViewById(R.id.gridview);
+        un = "DESKTOP-QPH6I1J/user";
+        passwords = "";
+        db = "restaurant";
 
         connect = CONN(un, passwords, db, ip);
         String query = "select * from countries";
 
         try {
             connect = CONN(un, passwords, db, ip);
-            Statement statement = connect.createStatement();
-            rs = statement.executeQuery(query);
-            List<Map<String, String>> data;
-            data = new ArrayList<>();
-
-            while (rs.next()) {
-                Map<String, String> datanum = new HashMap<>();
-                datanum.put("A", rs.getString("CustomerID"));
-                datanum.put("B", rs.getString("TableNo"));
-
-                data.add(datanum);
-            }
-
-            String[] from = {"A", "B"};
-            int[] views = {R.id.txtcountry, R.id.txtcontinent};
-            final SimpleAdapter ADA = new SimpleAdapter(MainActivity.this,
-                    data, R.layout.templateforgrid, from, views);
-            gridview.setAdapter(ADA);
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Log.i("SuccessfulConnection", "The connection with the database has been established");
+        } catch (Exception e) {
+            Log.e("ConnectionFailure", "Unable to connect to the Database");
         }
     }
 
+    //MSSQLSERVER - instance name
+
     @SuppressLint("NewApi")
-    private Connection CONN(String _user, String _pass, String _DB,
-                            String _server) {
+    private Connection CONN(String _user, String _pass, String _DB, String _server) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection conn = null;
-        String ConnURL;
+        String connURL = null;
         try {
 
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnURL = "jdbc:jtds:sqlserver://" + _server + ";"
-                    + "databaseName=" + _DB + ";user=" + _user + ";password="
-                    + _pass + ";";
-            conn = DriverManager.getConnection(ConnURL);
+
+            //ConnURL
+            connURL = "jdbc:jtds:sqlserver://sql5050.site4now.net;database=DB_A512F2_restaurant;user=DB_A512F2_restaurant_admin;password=dominic1234";
+            conn = DriverManager.getConnection(connURL);
         } catch (SQLException se) {
             Log.e("ERRO", se.getMessage());
         } catch (ClassNotFoundException e) {
@@ -106,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return conn;
     }
-
 
     public void openMenu() {
         Intent intent = new Intent(this, Main2Activity.class);
