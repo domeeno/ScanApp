5684b2ac262bcbbd345f32057d75675636f3cc76
dominatorfaf <45204549+dominatorfaf@users.noreply.github.com>
2020-01-16 01:41:12 +0200

    Fully working ExpandableListView

--- app/src/main/java/com/example/scanapp/Functional/FetchDataQuery.java
+++ app/src/main/java/com/example/scanapp/Functional/FetchDataQuery.java
@@ -7,12 +7,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class FetchDataQuery {
     private ArrayList<String> list;
-    List<String> elementList = new ArrayList<>();
     private String query;
     private HashMap<String, List<String>> subList;
     private Connection connect;
@@ -39,22 +39,29 @@ public class FetchDataQuery {
                 Log.e("NoData", "No data found, check connection or the db");
             }
 
-//            for (int i = 0; i < list.size(); i++) {
-                Statement stmt = connect.createStatement();
-                ResultSet rs1 = stmt.executeQuery("select Dishname from Dishes D1 left join  DishDetails d2\n" +
-                        "on d1.DishID = d2.DishID\n" +
-                        "WHERE CategoryID = (Select CategoryID from DishCategories where CategoryName = '" + list.get(0) + "');");
+            List<List<String>> array = new ArrayList<List<String>>();
 
-                if (rs1 != null) {
-                    while (rs1.next()) {
-                        elementList.add(rs1.getString("DishName"));
+            for (int i = 0; i < list.size(); i++) {
+                array.add(new ArrayList<String>());
+            }
+
+            Log.i("ArraySize", "Array size" + array.get(0).size());
+
+            for ( int i = 0; i < list.size(); i++) {
+                    Statement stmt = connect.createStatement();
+                    ResultSet rs1 = stmt.executeQuery("select Dishname from Dishes D1 left join  DishDetails d2\n" +
+                            "on d1.DishID = d2.DishID\n" +
+                            "WHERE CategoryID = (Select CategoryID from DishCategories where CategoryName = '" + list.get(i) + "');");
+
+                    if (rs1 != null) {
+                        while (rs1.next()) {
+                            array.get(i).add(rs1.getString("DishName"));
+                        }
+                    } else {
+                        Log.e("NoData", "No data found, check connection or the db");
                     }
-                } else {
-                    Log.e("NoData", "No data found, check connection or the db");
-                }
-//            }
-
-            subList.put(list.get(0), elementList);
+                subList.put(list.get(i), array.get(i));
+            }
 
         } catch (SQLException e) {
             Log.e("ConnectionFailure", "Connection Failure " + query);
